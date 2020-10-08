91a3346de719abcf0150fac7a7cb154f924050d7
gtongy <32930281+gtongy@users.noreply.github.com>
2020-10-09 00:10:31 +0900

    split snapshot and slack notify for test

--- __tests__/handler.test.ts
+++ __tests__/handler.test.ts
@@ -1,49 +1,71 @@
-import { getEnableEmbedDashboards } from '../handler';
+import { getEnableEmbedDashboards, snapShotMetabaseGraph } from '../handler';
 import axios from 'axios';
+
+const chromeLambda = require('chrome-aws-lambda');
 
 jest.mock('axios');
 
-test('has enable_embedding false', async () => {
-  const response = {
-    data: [
-      {
-        id: 1,
-        name: 'dashboard1',
-        enable_embedding: true
-      },
-      {
-        id: 2,
-        name: 'dashboard2',
-        enable_embedding: false
-      },
-      {
-        id: 3,
-        name: 'dashboard3',
-        enable_embedding: true
+describe('getEnableEmbedDashboards', () => {
+  beforeEach(() => {
+    console.error = jest.fn();
+  });
+  test('has enable_embedding false', async () => {
+    const response = {
+      data: [
+        {
+          id: 1,
+          name: 'dashboard1',
+          enable_embedding: true
+        },
+        {
+          id: 2,
+          name: 'dashboard2',
+          enable_embedding: false
+        },
+        {
+          id: 3,
+          name: 'dashboard3',
+          enable_embedding: true
+        }
+      ]
+    };
+    const mockedAxios = axios as jest.Mocked<typeof axios>;
+    mockedAxios.get.mockResolvedValue(response);
+    const embedDashboards = await getEnableEmbedDashboards();
+    expect(embedDashboards).toEqual([
+      { id: 1, name: 'dashboard1' },
+      { id: 3, name: 'dashboard3' }
+    ]);
+  });
+
+  test('has enable_embedding false', async () => {
+    const error = {
+      response: {
+        status: 500,
+        data: {
+          error: 'server error'
+        }
       }
-    ]
-  };
-  const mockedAxios = axios as jest.Mocked<typeof axios>;
-  mockedAxios.get.mockResolvedValue(response);
-  const embedDashboards = await getEnableEmbedDashboards();
-  expect(embedDashboards).toEqual([
-    { id: 1, name: 'dashboard1' },
-    { id: 3, name: 'dashboard3' }
-  ]);
+    };
+    const mockedAxios = axios as jest.Mocked<typeof axios>;
+    mockedAxios.get.mockRejectedValueOnce(error);
+    process.on('unhandledRejection', console.dir);
+    const dashboards = await getEnableEmbedDashboards();
+    expect(dashboards).toEqual([]);
+  });
 });
 
-test('has enable_embedding false', async () => {
-  const error = {
-    response: {
-      status: 401,
-      data: {
-        error: 'Unauthorized!'
-      }
-    }
+describe('snapShotMetabaseGraph', () => {
+  const screenshot = Buffer.alloc(20, 'Test');
+  const mockedPage = {
+    goto: jest.fn(),
+    waitFor: jest.fn(),
+    screenshot: jest.fn().mockResolvedValue(screenshot)
   };
-  const mockedAxios = axios as jest.Mocked<typeof axios>;
-  mockedAxios.get.mockRejectedValueOnce(error);
-  process.on('unhandledRejection', console.dir);
-  const dashboards = await getEnableEmbedDashboards();
-  expect(dashboards).toEqual([]);
+  const mockedBrowser = { newPage: jest.fn().mockResolvedValue(mockedPage), close: jest.fn() };
+  jest.spyOn(chromeLambda.puppeteer, 'launch').mockResolvedValue(mockedBrowser);
+  test('success snapshot', async () => {
+    const result = await snapShotMetabaseGraph('http://test.com');
+    expect(result).toEqual(screenshot);
+  });
 });
--- handler.ts
+++ handler.ts
@@ -3,7 +3,7 @@ import axios, { AxiosResponse } from 'axios';
 import jwt from 'jsonwebtoken';
 import { WebClient } from '@slack/web-api';
 import 'source-map-support/register';
-import log from 'lambda-log';
+const lambdalog = require('lambda-log');
 
 const chromeLambda = require('chrome-aws-lambda');
 
@@ -23,39 +23,14 @@ export const metabaseSlackNotify: ScheduledHandler = async () => {
     };
     const token = jwt.sign(payload, process.env.METABASE_SECRET_KEY);
     const embedUrl = process.env.METABASE_SITE_URL + '/embed/dashboard/' + token + '#bordered=true&titled=true';
-    const defaultViewport = {
-      width: 2000,
-      height: 100
-    };
     const readFontProcess = chromeLambda.font(
       'https://raw.githack.com/minoryorg/Noto-Sans-CJK-JP/master/fonts/NotoSansCJKjp-Regular.ttf'
     );
     await readFontProcess;
-    const browser = await chromeLambda.puppeteer.launch({
-      args: chromeLambda.args,
-      executablePath: await chromeLambda.executablePath,
-      headless: chromeLambda.headless,
-      ignoreHTTPSErrors: true,
-      defaultViewport
-    });
-
-    const page = await browser.newPage();
-    await page.goto(embedUrl);
-    await page.waitFor(5000);
-    const buffer = await page.screenshot({ fullPage: true });
-    await browser.close();
-    try {
-      const web = new WebClient(process.env.SLACK_TOKEN);
-      await web.files.upload({
-        filename: `${dashboard.name}.png`,
-        file: buffer,
-        channels: process.env.SLACK_CHANNELS
-      });
-    } catch (error) {
-      log.error(error.response.data);
-    }
+    const buffer = await snapShotMetabaseGraph(embedUrl);
+    await slackFileNotify(dashboard, buffer);
   }
-  log.info('metabase slack notified!');
+  lambdalog.info('metabase slack notified!');
 };
 
 export const getEnableEmbedDashboards = async (): Promise<EmbedDashboard[]> => {
@@ -67,11 +42,44 @@ export const getEnableEmbedDashboards = async (): Promise<EmbedDashboard[]> => {
   try {
     response = await axios.get(`${process.env.METABASE_SITE_URL}/api/dashboard`, { headers });
   } catch (error) {
+    lambdalog.error(error.response.data);
     return [];
   }
   const enableEmbedDashboards = response.data
     .filter(item => item.enable_embedding === true)
     .map(dashboard => ({ id: dashboard.id, name: dashboard.name }));
-  if (enableEmbedDashboards.length <= 0) log.Info('dashboard is not exist');
+  if (enableEmbedDashboards.length <= 0) lambdalog.Info('dashboard is not exist');
   return enableEmbedDashboards;
+};
+
+export const snapShotMetabaseGraph = async (
+  embedUrl: string,
+  defaultViewport: { width: number; height: number } = { width: 2000, height: 100 }
+): Promise<Buffer> => {
+  const browser = await chromeLambda.puppeteer.launch({
+    args: chromeLambda.args,
+    executablePath: await chromeLambda.executablePath,
+    headless: chromeLambda.headless,
+    ignoreHTTPSErrors: true,
+    defaultViewport
+  });
+  const page = await browser.newPage();
+  await page.goto(embedUrl);
+  await page.waitFor(5000);
+  const buffer = await page.screenshot({ fullPage: true });
+  await browser.close();
+  return buffer;
+};
+
+export const slackFileNotify = async (dashboard: EmbedDashboard, buffer: Buffer) => {
+  try {
+    const web = new WebClient(process.env.SLACK_TOKEN);
+    await web.files.upload({
+      filename: `${dashboard.name}.png`,
+      file: buffer,
+      channels: process.env.SLACK_CHANNELS
+    });
+  } catch (error) {
+    lambdalog.error(error.response.data);
+  }
 };
